cec327b4a372f20a876daba26c7740392768840c
goracz.akos <akos.zoltan.goracz@gmail.com>
2016-06-08 09:57:11 +0200

    kompetencia cascade

--- app/models/competence.rb
+++ app/models/competence.rb
@@ -2,9 +2,9 @@ class Competence < ActiveRecord::Base
   belongs_to :competence_type
 
   
-  has_many :pending_competence_levels
+  has_many :pending_competence_levels, dependent: :delete_all
   has_many :pending_users, through: :pending_competence_levels, class_name: 'User', source: :user
   
-  has_many :assigned_competence_levels
+  has_many :assigned_competence_levels, dependent: :delete_all
   has_many :users, through: :assigned_competence_levels
 end
