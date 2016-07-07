5106b07f23a40ab4d225e5ef6e7a08d1ff6258ed
goracz.akos <akos.zoltan.goracz@gmail.com>
2016-07-07 05:39:38 +0200

    query potencialis fix

--- app/models/saved_query.rb
+++ app/models/saved_query.rb
@@ -1,5 +1,5 @@
 class SavedQuery < ActiveRecord::Base
-  has_many :saved_query_competences
+  has_many :saved_query_competences, dependent: :destroy
   has_many :competences, through: :saved_query_competences
   
   has_many :report_saved_queries, dependent: :destroy
@@ -8,7 +8,7 @@ class SavedQuery < ActiveRecord::Base
   def competence_query_params
     competence_params=self.saved_query_competences.map do |c|
       {
-        'competence_id' => c.id,
+        'competence_id' => c.competence_id,
         'level' => c.level
       }
     end
